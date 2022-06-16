db719278fa4a69cf9634a77632b20bd31812ee8f
vova11 <vladimir.zembera@gmail.com>
2022-06-16 17:28:41 +0200

    fixed before install script9

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Stopping existing nodsse server
 echo "Copy file"
-cp -v /home/ec2-user/app.env /home/ec2-user/scam-web-manger/src/
+sudo cp -v /home/ec2-user/app.env /home/ec2-user/scam-web-manger/src/
