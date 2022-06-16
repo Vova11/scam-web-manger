878766c72cff17ef0653ba55091b157021ba6528
vova11 <vladimir.zembera@gmail.com>
2022-06-16 15:29:56 +0200

    fixed before install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Before install script"
-cp /home/ec2-user/app.env /home/ec2-user/scam-web-manger/src/scam-web-manger/src/
+cp /home/ec2-user/app.env /home/ec2-user/scam-web-manger/src/
 
 DIR="/home/ec2-user/scam-web-manger"
 if [-d "$DIR" ]; then
