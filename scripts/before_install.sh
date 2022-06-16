6eee77a6e17754f7f1e99e52c9fa3332e01dda6b
vova11 <vladimir.zembera@gmail.com>
2022-06-16 17:25:33 +0200

    fixed before install script8

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 #Stopping existing nodsse server
-DIR="/home/ec2-user/scam-web-manger"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
-
-echo "Copy filer"
+echo "Copy file"
 cp -v /home/ec2-user/app.env /home/ec2-user/scam-web-manger/src/
