1dc4778a29002fdae0ff1c4e1774e7db07f278e8
vova11 <vladimir.zembera@gmail.com>
2022-06-16 14:54:39 +0200

    app start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Changing app user..."
 sudo chown -R ec2-user:ec2-user /home/ec2-user/scam-web-manger
+echo "run npm install ..."
+npm install
 echo "Starting application"
 sudo systemctl start nginx
 sudo systemctl start expressapp.service
